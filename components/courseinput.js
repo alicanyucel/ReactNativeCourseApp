edd324be7468d0f44ce2dc902fe89de095c43810
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-04-14 20:17:14 +0300

    modal....

--- components/courseinput.js
+++ components/courseinput.js
@@ -1,11 +1,26 @@
-import { StyleSheet,Text,View } from "react-native";
+import { StyleSheet, Text, View, Modal, Alert } from "react-native";
 import React from "react";
-export default function CourseInput({visible})
-{
-    return(
-        <View>
-            <Text>CourseInput</Text>
-        </View>
+export default function CourseInput({ visible }) {
+    return (
+        <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+                Alert.alert('Modal has been closed.');
+                setModalVisible(!modalVisible);
+            }}>
+            <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+                    <Text style={styles.modalText}>Merhaba Dünya</Text>
+                    <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => setModalVisible(!modalVisible)}>
+                        <Text style={styles.textStyle}>Gizle</Text>
+                    </Pressable>
+                </View>
+            </View>
+        </Modal>
     );
 }
-const styles=StyleSheet.create({});
+const styles = StyleSheet.create({});
