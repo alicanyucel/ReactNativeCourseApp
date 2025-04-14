10bdc9958fd1e627e038324737c4dea4d37a4cf2
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-04-14 19:35:09 +0300

    STYLE EKLENDİ.

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View ,Button} from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { useState } from 'react';
 import CourseInput from './components/courseinput';
 export default function App() {
-  const [modalIsVisibe,setModalIsVisible]=useState(false);
-  const startModal=()=>{
+  const [modalIsVisibe, setModalIsVisible] = useState(false);
+  const startModal = () => {
     setModalIsVisible(true);
   }
   return (
     <>
-     <StatusBar style="light" />
-    <View style={styles.container}>
-      <Text>Kurslarım Uygulaması</Text>
-     <Button title='Kurs Ekle' color="red" onPress={startModal}/>
-     <CourseInput  visible={modalIsVisibe}/>
-    </View>
+      <StatusBar style="light" />
+      <View style={styles.container}>
+        <Text style={styles.title}>Ali Can Yücel Course App</Text>
+        <Button
+          title='Kurs Ekle'
+          color="red"
+          onPress={startModal}
+          style={styles.button}
+        />
+        <CourseInput visible={modalIsVisibe} />
+      </View>
     </>
   );
 }
@@ -23,7 +28,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop:50,
-    paddingHorizontal:20,
+    paddingTop: 80,
+    paddingHorizontal: 30,
+  },
+  title: {
+    marginBottom: 10,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  button: {
+    marginBottom: 40,
   },
 });
